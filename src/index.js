5130cbc970d42f6c4dbf17a497ca8fce36ae8446
Arone Jerome <aronemalicse@gmail.com>
2019-09-18 14:25:46 +0800

    with get time

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
+//Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//eslint-disable-next-line
+function getButtonText(params) {
+    return "Click On Me!";
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function getTime() {
+    return (new Date()).toLocaleTimeString()
+}
+
+//Create a react component
+const App = function () {
+    const buttonText = { Text1: "Click1", Text2: "Click2"};
+    const labelText = "Enter name here: ";
+    return <div>
+    <label className="label" htmlFor="name">
+        {labelText}
+    </label>
+    <input id="name" type="text"/>
+    <button style={{border: "1px solid red", backgroundColor: "blue", color: "white"}}>
+        {buttonText.Text2}
+    </button>
+    <div>
+        Current Time:
+    </div>
+    <h3>{getTime()}</h3>
+  </div>
+}
+
+//Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
